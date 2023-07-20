19e27c98a8d7ebf1a4a5d674619aec8f31e8024b
nick95a <nick95a@yandex.ru>
2023-07-20 20:28:02 +0500

    first version of the language test, but with page object model

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+class BasePage():
+    # Инициализации класса с конструктором
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+    
+    def open(self):
+        self.browser.get(self.url)
